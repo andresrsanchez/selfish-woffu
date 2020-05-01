8971bdb9c8141b94cbf0d9ac09b717c44e72e70d
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2020-05-01 22:46:33 +0200

    add test project

---
+++ tests/woffu.core.tests/TimeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace woffu.core.tests
+{
+    public class time_should
+    {
+        [Fact]
+        public void create_class()
+        {
+
+        }
+    }
+}
